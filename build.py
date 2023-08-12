0826f37c26593d7508df5f28af1088288ff85e14
chonker <36255253+Derro8@users.noreply.github.com>
2023-08-11 20:13:22 -0700

    Add files via upload

--- build.py
+++ build.py
@@ -13,6 +13,18 @@ if len(args) > 0:
 else:
     directory = input("Extension folder: ")
 
+def xpidir(path, version):
+    ziph = zipfile.ZipFile(f"{directory}\\builds\\{os.path.basename(directory)}-{version}.xpi", "w")
+    for root, dirs, files in os.walk(path):
+        if "builds" in root:
+            continue
+        
+        # with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zip_ref:
+    # for folder_name, subfolders, filenames in os.walk(name):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            ziph.write(file_path, arcname=file_path.replace(directory, ""))
+
 if os.path.exists(directory) and os.path.isdir(directory):
     with open(f"{directory}\\manifest.json", "r") as f:
         js: dict = json.loads(f.read())
@@ -32,12 +44,4 @@ if os.path.exists(directory) and os.path.isdir(directory):
             with open(f"{directory}\\manifest.json", "w") as fw:
                 fw.write(json.dumps(js, indent=4))
 
-        shutil.make_archive(f"{directory}\\builds\\{os.path.basename(directory)}-{js['version']}", "zip", directory)
-        with zipfile.ZipFile(f"{directory}\\builds\\{os.path.basename(directory)}-{js['version']}.zip", "r") as zip:
-            with zipfile.ZipFile(f"{directory}\\builds\\{os.path.basename(directory)}-{js['version']}.xpi", "w") as xpi:
-                for info in zip.filelist:
-                    buffer = zip.read(info.filename)
-                    if info.filename[:7] != "builds/":
-                        xpi.writestr(info, buffer)
-        
-        os.remove(f"{directory}\\builds\\{os.path.basename(directory)}-{js['version']}.zip")
+        xpidir(f"{directory}", js["version"])

7643bba06f810f289494a36b8d8c047f4d7c28b3
chonker <36255253+Derro8@users.noreply.github.com>
2023-08-11 17:48:51 -0700

    Add files via upload

--- crunchyroll-profiles/src/profile.js
+++ crunchyroll-profiles/src/profile.js
@@ -33,7 +33,7 @@ request.override([URLS.profile], "GET", async (info) => {
     storage.currentUser = profiles.current
     return storage.get(storage.currentUser, "profile", (profile) => {
       if(profile === undefined) {
-        storage.set(storage.currentUser, "profile", profile);
+        storage.set(storage.currentUser, "profile", JSON.parse(info.body));
         return info.body;
       }
       
